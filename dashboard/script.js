1bf60a50f1a3d0ac8829952f1803caee3a04afcb
Chandana S <chandanasunith1203@gmail.com>
2024-10-19 23:56:25 +0530

    Add files via upload

---
+++ dashboard/script.js
@@ -0,0 +1,5 @@
+function executePromptFunction() {
+    const inputValue = document.querySelector('.prompt-input').value;
+    alert('Executing: ' + inputValue); // Replace with actual functionality
+  }
+  
